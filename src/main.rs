9e9d93c5b6a1ebd81370c910c70ca794b19eae67
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2017-08-03 21:56:34 +0200

    Handle terminal resize

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate term_mux;
 extern crate termion;
+extern crate chan_signal;
 
 use std::io::{Read, Write, Result};
 use std::fs::File;
@@ -7,21 +8,26 @@ use std::thread;
 use std::time::Duration;
 use termion::get_tty;
 use termion::raw::IntoRawMode;
+use chan_signal::{notify, Signal};
 use term_mux::pty::Pty;
 use term_mux::tui::{get_terminal_size, Size};
 use term_mux::get_shell;
 
 fn main () {
+    let signal = notify(&[Signal::WINCH]);
+    let pty_resize = Pty::spawn(&get_shell(), &get_terminal_size().unwrap()).unwrap();
+
     let mut tty_output = get_tty().unwrap().into_raw_mode().unwrap();
-    let mut pty_output = Pty::spawn(&get_shell(), &get_terminal_size().unwrap()).unwrap();
+    let mut pty_output = pty_resize.try_clone().unwrap();
 
     let mut tty_input = tty_output.try_clone().unwrap();
     let mut pty_input = pty_output.try_clone().unwrap();
 
+
     let handle1 = thread::spawn(move || {
         loop {
             let tty_file: &mut File = &mut tty_output;
-            match pipe_input_to_output::<Pty, File>(&mut pty_input, tty_file) {
+            match pipe::<Pty, File>(&mut pty_input, tty_file) {
                 Err(_) => return,
                 _      => (),
             }
@@ -31,17 +37,25 @@ fn main () {
     let handle2 = thread::spawn(move || {
         loop {
             let tty_file: &mut File = &mut tty_input;
-            match pipe_input_to_output::<File, Pty>(tty_file, &mut pty_output) {
+            match pipe::<File, Pty>(tty_file, &mut pty_output) {
                 Err(_) => return,
                 _      => (),
             }
         }
     });
 
+    let handle3 = thread::spawn(move || {
+        loop {
+            signal.recv().unwrap();
+            pty_resize.resize(&get_terminal_size().unwrap());
+        }
+    });
+
     handle1.join();
 }
 
-fn pipe_input_to_output<R: Read, W: Write>(input: &mut Read, output: &mut Write) -> Result<()> {
+/// Sends the content of input into output
+fn pipe<R: Read, W: Write>(input: &mut Read, output: &mut Write) -> Result<()> {
     let mut packet = [0; 4096];
 
     let count = input.read(&mut packet)?;
